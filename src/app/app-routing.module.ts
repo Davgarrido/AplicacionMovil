183652a9d75966d996559444610b5089f7f0ae86
SEBASTIANTAPIAARAVENA <127543450+SEBASTIANTAPIAARAVENA@users.noreply.github.com>
2023-10-20 21:41:12 -0300

    cambios not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,30 +6,30 @@ const routes: Routes = [
   {
     path: 'home',
     canActivate: [FirstGuardGuard],
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'Login',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'reestablecer',
+    loadChildren: () => import('./reestablecer/reestablecer.module').then(m => m.ReestablecerPageModule)
+  },
+  {
     path: 'not-found',
-    loadChildren: () => import('./not-found/not-found.module').then( m => m.NotFoundPageModule)
+    loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundPageModule)
   },
   {
     path: '**',
     redirectTo: '/not-found',
     pathMatch: 'full',
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'reestablecer',
-    loadChildren: () => import('./reestablecer/reestablecer.module').then( m => m.ReestablecerPageModule)
-  },
 ];
 
 @NgModule({
