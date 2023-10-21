3a73bf247b3d2de79f0d631a068ebe5f57882473
SEBASTIANTAPIAARAVENA <127543450+SEBASTIANTAPIAARAVENA@users.noreply.github.com>
2023-10-21 01:19:18 -0300

    borrado de parentesis async

--- src/app/reestablecer/reestablecer.page.spec.ts
+++ src/app/reestablecer/reestablecer.page.spec.ts
@@ -5,11 +5,11 @@ describe('ReestablecerPage', () => {
   let component: ReestablecerPage;
   let fixture: ComponentFixture<ReestablecerPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async() => {
     fixture = TestBed.createComponent(ReestablecerPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
