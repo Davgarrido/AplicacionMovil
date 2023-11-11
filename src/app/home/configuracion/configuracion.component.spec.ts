5daeee39ddd768f2225f2a8b62a7044d9f1cab48
SEBASTIANTAPIAARAVENA <127543450+SEBASTIANTAPIAARAVENA@users.noreply.github.com>
2023-11-11 10:02:19 -0300

    cambios profe

--- src/app/home/configuracion/configuracion.component.spec.ts
+++ src/app/home/configuracion/configuracion.component.spec.ts
@@ -2,16 +2,17 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ConfiguracionComponent } from './configuracion.component';
+import { RouterModule } from '@angular/router';
 
 describe('ConfiguracionComponent', () => {
   let component: ConfiguracionComponent;
   let fixture: ComponentFixture<ConfiguracionComponent>;
 
   beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+    /*TestBed.configureTestingModule({
       declarations: [ ConfiguracionComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      imports: [IonicModule.forRoot(),RouterModule]
+    }).compileComponents();*/
 
     fixture = TestBed.createComponent(ConfiguracionComponent);
     component = fixture.componentInstance;
--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { ActivatedRoute } from '@angular/router';
 
 describe('HomePage', () => {
   let component: HomePage;
